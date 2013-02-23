89cfcfc048fcd42e1477801e2a303725acb10f19
Lars Greiss <kufikugel@googlemail.com>
2013-02-23 02:28:10 +0100

    Remove cLock and add DashClock
    we remove cLock completly....we will left only the ability in settings
    that cLock can show his settings if installed. cLock will be provided
    by us as extra package in future.
    
    Thanks to crazy dev roman nurik
    
    Change-Id: I4df61beb08c8928fc723447b2d1fad951ee2c960

--- config/common.mk
+++ config/common.mk
@@ -137,7 +137,7 @@ PRODUCT_PACKAGES += \
 # Extra Optional packages
 PRODUCT_PACKAGES += \
     HoloLauncherHD \
-    LockClock \
+    DashClock \
     SlimFileManager \
     SlimOTA \
     Velvet \

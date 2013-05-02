738a6b4e86cd51902e4f84df6cc3b3b2a508d300
Lars Greiss <kufikugel@googlemail.com>
2013-05-02 22:48:22 +0200

    Codefreeze SlimBean.4.2.2.build.5
    final code freeze for release
    
    really awesome work...seems to be that we have
    now the most stable SlimBean build since first 4.2
    release. Great job from all!!!!!
    
    goooooooooooooo Slim
    
    cheers
    
    Change-Id: Ib5d4616c4f173ed6979a06b21e5177716ea25fa5

--- config/common.mk
+++ config/common.mk
@@ -106,10 +106,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/slim/overlay/common
 include vendor/slim/config/themes_common.mk
 
 # Versioning System
-# Pre codefreeze 4.2.2.build.5
+# Codefreeze SlimBean.4.2.2.build.5
 PRODUCT_VERSION_MAJOR = 4.2.2
 PRODUCT_VERSION_MINOR = build
-PRODUCT_VERSION_MAINTENANCE = 5.PCFR
+PRODUCT_VERSION_MAINTENANCE = 5
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(BUILD_ID)
 

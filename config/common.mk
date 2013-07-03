1c72413639956e51d1529f2a601217487d505baf
Lars Greiss <kufikugel@googlemail.com>
2013-07-03 20:10:50 +0200

    Final freeze build.7
    awesome awesome job guys
    
    Change-Id: I4c6f5e1a6ffd773952f7178a012ab399ca84a571

--- config/common.mk
+++ config/common.mk
@@ -96,10 +96,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/slim/overlay/common
 include vendor/slim/config/themes_common.mk
 
 # Versioning System
-# Pre Code freeze build.7
+# Freeze build.7
 PRODUCT_VERSION_MAJOR = 4.2.2
 PRODUCT_VERSION_MINOR = build
-PRODUCT_VERSION_MAINTENANCE = 7.PCFR
+PRODUCT_VERSION_MAINTENANCE = 7
 ifdef SLIM_BUILD_EXTRA
     SLIM_POSTFIX := -$(SLIM_BUILD_EXTRA)
 endif

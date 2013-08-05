95d342ea571b165c5d7824c951fb7a333435516f
fronti90 <m90braun@gmail.com>
2013-08-05 21:12:02 +0200

    add geefhd_att_us
    Change-Id: Id97953b362ad7255659186a0a86265d996f09956

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,7 @@ add_lunch_combo slim_endeavoru-userdebug
 add_lunch_combo slim_flo-userdebug
 add_lunch_combo slim_galaxysmtd-userdebug
 add_lunch_combo slim_gkatt-userdebug
+add_lunch_combo slim_geefhd_att_us-userdebug
 add_lunch_combo slim_grouper-userdebug
 add_lunch_combo slim_hercules-userdebug
 add_lunch_combo slim_i777-userdebug

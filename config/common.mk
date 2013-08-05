6daac0aad7be13b0cbfeeb2eb47e94cc92603b40
Lars Greiss <kufikugel@googlemail.com>
2013-08-05 06:38:08 +0200

    enable selinux in settings
    Change-Id: I5c5f3a95e6d9a639c203cdad9b686e816c12ab67

--- config/common.mk
+++ config/common.mk
@@ -10,6 +10,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \

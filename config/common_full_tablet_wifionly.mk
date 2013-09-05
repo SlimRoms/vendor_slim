55e7b52334935c960a491cd89e48588d28d8b488
Lars Greiss <kufikugel@googlemail.com>
2013-09-05 19:21:39 +0200

    Remove bluetooth configs
    we do not need them any more and can inteference with our
    current bt implemenation
    
    Change-Id: Iadcb4c1eb022241c4759e09af6ff077f95c98aa1

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,6 +1,2 @@
 # Inherit common stuff
 $(call inherit-product, vendor/slim/config/common.mk)
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf

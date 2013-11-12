b2e7009b548729ace6b2890238eafe478b806ec7
cordell <cordellsmail@gmail.com>
2013-11-12 22:51:19 +0100

    world phone suport
    Change-Id: Ib08ea454db046aed641cc748cfb3733e9bc000ce

--- config/cdma.mk
+++
@@ -1,3 +0,0 @@
-# CDMA APN list
-PRODUCT_COPY_FILES += \
-    vendor/slim/prebuilt/common/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,2 +1,14 @@
 # Inherit common stuff
 $(call inherit-product, vendor/slim/config/common.mk)
+
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk

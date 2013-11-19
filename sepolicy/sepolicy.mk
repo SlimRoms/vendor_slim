2564453e342bd6071783988dc114e1c2f9efb735
Lars Greiss <kufikugel@googlemail.com>
2013-11-19 09:26:39 +0100

    vendor: several selinux updates
    to support all our devices and slims system
    
    Change-Id: Ia4c74eca1c520554b7ae0ab068fa7e8742b00f98

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,11 +1,16 @@
 #
 # This policy configuration will be used by all products that
-# inherit from SLIM
 #
 
 BOARD_SEPOLICY_DIRS += \
     vendor/slim/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+    file.te \
     file_contexts \
+    fs_use \
+    genfs_contexts \
+    installd.te \
+    seapp_contexts \
+    vold.te \
     mac_permissions.xml

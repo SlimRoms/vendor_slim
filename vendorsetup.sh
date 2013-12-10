fe05dbe13fee9f9de6c6ba50610ea10d70dba3c8
Tudor Sirb <tudor@sirb.net>
2013-12-10 17:29:13 +0400

    Add p700 to vendorsetup
    Change-Id: I7ecf547fba4be9f2c7884f1d860c2dd4a10fd737

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -49,3 +49,4 @@ add_lunch_combo slim_toroplus-userdebug
 add_lunch_combo slim_vibrantmtd-userdebug
 add_lunch_combo slim_xt925-userdebug
 add_lunch_combo slim_xt926-userdebug
+add_lunch_combo slim_p700-userdebug

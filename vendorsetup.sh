a9bcaa129d54a096bb6a1be25e7e7ea2a1737463
Griffin Millender <griffinn.millender@gmail.com>
2013-12-18 09:20:02 -0600

    Add g2 variants to vendorsetup
    Change-Id: I698814e2f4cd9ccc2797768fae7cf387fd95c639

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,9 @@ add_lunch_combo slim_d2att-userdebug
 add_lunch_combo slim_d2spr-userdebug
 add_lunch_combo slim_d2usc-userdebug
 add_lunch_combo slim_d2vzw-userdebug
+add_lunch_combo slim_d800-userdebug
+add_lunch_combo slim_d801-userdebug
+add_lunch_combo slim_d802-userdebug
 add_lunch_combo slim_deb-userdebug
 add_lunch_combo slim_e610-userdebug
 add_lunch_combo slim_endeavoru-userdebug
@@ -25,6 +28,7 @@ add_lunch_combo slim_jfltespr-userdebug
 add_lunch_combo slim_jfltetmo-userdebug
 add_lunch_combo slim_jfltevzw-userdebug
 add_lunch_combo slim_jfltexx-userdebug
+add_lunch_combo slim_ls980-userdebug
 add_lunch_combo slim_m7att-userdebug
 add_lunch_combo slim_m7spr-userdebug
 add_lunch_combo slim_m7tmo-userdebug
@@ -49,6 +53,7 @@ add_lunch_combo slim_tilapia-userdebug
 add_lunch_combo slim_toro-userdebug
 add_lunch_combo slim_toroplus-userdebug
 add_lunch_combo slim_vibrantmtd-userdebug
+add_lunch_combo slim_vs980-userdebug
 add_lunch_combo slim_xt925-userdebug
 add_lunch_combo slim_xt926-userdebug
 

7130751405ee48ab8b8746a350c42a10746a217f
rebelos <ks50497@gmail.com>
2014-11-06 17:03:16 +0000

    Revert "slim: Build stagefright ffmpeg plugin"
    * Stop Building ffmpeg pulgin until it gets fixed
    This reverts commit 76f7bccf0a8a0d5915f064e0e847cb4cab9b26c1.
    Change-Id: I23d50a299bee724ab523723a12df6cd1ff0f3a1a

--- config/common.mk
+++ config/common.mk
@@ -104,13 +104,6 @@ PRODUCT_PACKAGES += \
     fsck.exfat \
     mkfs.exfat
 
-# Stagefright FFMPEG plugin
-PRODUCT_PACKAGES += \
-    libstagefright_soft_ffmpegadec \
-    libstagefright_soft_ffmpegvdec \
-    libFFmpegExtractor \
-    libnamparser
-
 # easy way to extend to add more packages
 -include vendor/extra/product.mk
 

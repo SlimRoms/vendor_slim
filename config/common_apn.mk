4c686ae20e3936455b8e009cd1cf73085c203cd7
Nicholas Flintham <nick@flinny.org>
2015-01-19 14:55:04 +0000

    Create config for sim enabled tablets and split out generic apn configurations
    Change-Id: I6f953386dbc2592c230a849f1b7d6fd3df678add

---
+++ config/common_apn.mk
@@ -0,0 +1,12 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
--- config/common_full_tablet_lte.mk
+++ config/common_full_tablet_lte.mk
@@ -1,7 +1,3 @@
 # Inherit common stuff
 $(call inherit-product, vendor/slim/config/common.mk)
 $(call inherit-product, vendor/slim/config/common_apn.mk)
-
-# SIM Toolkit
-PRODUCT_PACKAGES += \
-    Stk

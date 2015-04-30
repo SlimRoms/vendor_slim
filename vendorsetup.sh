06942d36bf8fbde9a174f6fb16fd23c4824d5258
Anthony King <anthonydking@slimroms.net>
2015-05-01 00:00:37 +0100

    vendor: slim: change how device combos are added
    device names are now added to slim.devices.
    
    A custom device list file can be used by setting:
      export CUSTOM_DEVICE_LIST="path/1.devices path/2.devices"
    
    custom variants can be used by setting:
      export CUSTOM_VARIANTS="eng user userdebug"
    
    setting either of these overwrites the default values
    
    Change-Id: I12ddf088a1377233640c359dc2f4fb305018052e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,88 +1,21 @@
-add_lunch_combo slim_amami-userdebug
-add_lunch_combo slim_armani-userdebug
-add_lunch_combo slim_bacon-userdebug
-add_lunch_combo slim_crespo-userdebug
-add_lunch_combo slim_d2att-userdebug
-add_lunch_combo slim_d2mtr-userdebug
-add_lunch_combo slim_d2spr-userdebug
-add_lunch_combo slim_d2tmo-userdebug
-add_lunch_combo slim_d2usc-userdebug
-add_lunch_combo slim_d2vzw-userdebug
-add_lunch_combo slim_d800-userdebug
-add_lunch_combo slim_d801-userdebug
-add_lunch_combo slim_d802-userdebug
-add_lunch_combo slim_d803-userdebug
-add_lunch_combo slim_d850-userdebug
-add_lunch_combo slim_d851-userdebug
-add_lunch_combo slim_d852-userdebug
-add_lunch_combo slim_d855-userdebug
-add_lunch_combo slim_deb-userdebug
-add_lunch_combo slim_endeavoru-userdebug
-add_lunch_combo slim_falcon-userdebug
-add_lunch_combo slim_flo-userdebug
-add_lunch_combo slim_galaxysmtd-userdebug
-add_lunch_combo slim_gkatt-userdebug
-add_lunch_combo slim_geeb-userdebug
-add_lunch_combo slim_geefhd_att_us-userdebug
-add_lunch_combo slim_grouper-userdebug
-add_lunch_combo slim_hammerhead-userdebug
-add_lunch_combo slim_hlte-userdebug
-add_lunch_combo slim_hltespr-userdebug
-add_lunch_combo slim_hltetmo-userdebug
-add_lunch_combo slim_honami-userdebug
-add_lunch_combo slim_i777-userdebug
-add_lunch_combo slim_i9100-userdebug
-add_lunch_combo slim_i9100g-userdebug
-add_lunch_combo slim_i925-userdebug
-add_lunch_combo slim_i9300-userdebug
-add_lunch_combo slim_i9500-userdebug
-add_lunch_combo slim_jflteatt-userdebug
-add_lunch_combo slim_jfltecan-userdebug
-add_lunch_combo slim_jfltespr-userdebug
-add_lunch_combo slim_jfltetmo-userdebug
-add_lunch_combo slim_jflteusc-userdebug
-add_lunch_combo slim_jfltevzw-userdebug
-add_lunch_combo slim_jfltexx-userdebug
-add_lunch_combo slim_ls980-userdebug
-add_lunch_combo slim_ls990-userdebug
-add_lunch_combo slim_m7-userdebug
-add_lunch_combo slim_m7spr-userdebug
-add_lunch_combo slim_m7vzw-userdebug
-add_lunch_combo slim_m8-userdebug
-add_lunch_combo slim_maguro-userdebug
-add_lunch_combo slim_mako-userdebug
-add_lunch_combo slim_manta-userdebug
-add_lunch_combo slim_mondrianwifi-userdebug
-add_lunch_combo slim_moto_msm8960-userdebug
-add_lunch_combo slim_moto_msm8960dt-userdebug
-add_lunch_combo slim_n5100-userdebug
-add_lunch_combo slim_n5110-userdebug
-add_lunch_combo slim_n5120-userdebug
-add_lunch_combo slim_n7000-userdebug
-add_lunch_combo slim_n7100-userdebug
-add_lunch_combo slim_p3100-userdebug
-add_lunch_combo slim_p3110-userdebug
-add_lunch_combo slim_p5100-userdebug
-add_lunch_combo slim_p5110-userdebug
-add_lunch_combo slim_p700-userdebug
-add_lunch_combo slim_pyramid-userdebug
-add_lunch_combo slim_shamu-userdebug
-add_lunch_combo slim_sirius-userdebug
-add_lunch_combo slim_skyrocket-userdebug
-add_lunch_combo slim_t0lte-userdebug
-add_lunch_combo slim_t0lteatt-userdebug
-add_lunch_combo slim_t0ltetmo-userdebug
-add_lunch_combo slim_taoshan-userdebug
-add_lunch_combo slim_tilapia-userdebug
-add_lunch_combo slim_togari-userdebug
-add_lunch_combo slim_toro-userdebug
-add_lunch_combo slim_toroplus-userdebug
-add_lunch_combo slim_trltecan-userdebug
-add_lunch_combo slim_trltespr-userdebug
-add_lunch_combo slim_trltetmo-userdebug
-add_lunch_combo slim_trlteusc-userdebug
-add_lunch_combo slim_trltevzw-userdebug
-add_lunch_combo slim_v500-userdebug
-add_lunch_combo slim_vs980-userdebug
-add_lunch_combo slim_vs985-userdebug
-add_lunch_combo slim_yuga-userdebug
+cur_dir="$(gettop)/vendor/slim"
+
+list_files=(${cur_dir}/slim.devices)
+variants=(userdebug)
+[[ ${CUSTOM_DEVICE_LIST} ]] && list_files=(${CUSTOM_DEVICE_LIST})
+[[ ${CUSTOM_VARIANTS} ]] && variants=(${CUSTOM_VARIANTS})
+
+for list_file in "${list_files[@]}"
+do
+    if [[ -f "${list_file}" ]]
+    then
+        while IFS= read -r device
+        do
+            for variant in "${variants[@]}"
+            do
+                add_lunch_combo "slim_${device}-${variant}"
+            done
+        done < "${list_file}"
+    fi
+done
+

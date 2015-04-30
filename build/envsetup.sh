0aa0994c4ae84090c7a21ba5bfc00a4ae0617361
Anthony King <anthonydking@slimroms.net>
2015-05-01 00:00:37 +0100

    vendor: slim: change how device combos are added
    device names are now added to slim.devices.
    
    A custom device list file can be used by calling:
      slim_device_combos "path/custom.devices"
    
    A custom variant can be used by calling:
      slim_device_combos "eng"
    
    Both can be used by calling:
      slim_device_combos "path/custom.devices" "eng"
    
    setting either of these overwrites the default values
    
    This also introduces the start of our vendor envsetup
    Change-Id: I12ddf088a1377233640c359dc2f4fb305018052e

---
+++ build/envsetup.sh
@@ -0,0 +1,36 @@
+# slim functions that extend build/envsetup.sh
+
+function slim_device_combos() {
+    T="$(gettop)"
+    list_file="${T}/vendor/slim/slim.devices"
+    variant="userdebug"
+
+    if [[ $1 ]]
+    then
+        if [[ $2 ]]
+        then
+            list_file="$1"
+            variant=$2
+        else
+            if [[ ${VARIANT_CHOICES[@]} =~ (^| )$1($| ) ]]
+            then
+                variant=$1
+            else
+                list_file="$1"
+            fi
+        fi
+    fi
+
+    if [[ ! -f "$list_file" ]]
+    then
+        echo "unable to find device list: $list_file"
+        list_file="${T}/vendor/slim/slim.devices"
+        echo "defaulting device list file to: $list_file"
+    fi
+
+    while IFS= read -r device
+    do
+        add_lunch_combo "slim_${device}-${variant}"
+    done < "${list_file}"
+}
+

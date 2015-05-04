58c3dfb28a7c0b6beaa86598b58d6af341b6864d
Anthony King <anthonydking@slimroms.net>
2015-05-04 14:05:57 +0100

    slim: envsetup: add method for extending hmm descriptions
    This will replace or add an entry in the hmm descriptions,
    allowing us to cleanly extend hmm
    
    Change-Id: I6f79b83e95382cd839bcc587d022b81084febf9b

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -38,3 +38,21 @@ function slim_rename_function() {
     eval "original_slim_$(declare -f ${1})"
 }
 
+function slim_add_hmm_entry() {
+    f_name="${1}"
+    f_desc="${2}"
+
+    function _build_entry() {
+        printf "%-8s %s" "${f_name}:" "${f_desc}"
+    }
+
+    for c in ${!HMM_DESCRIPTIVE[*]}
+    do
+        if [[ "${f_name}" == $(echo "${HMM_DESCRIPTIVE[$c]}" | cut -f1 -d":") ]]
+        then
+            HMM_DESCRIPTIVE[${c}]="$(_build_entry)"
+            return
+        fi
+    done
+    HMM_DESCRIPTIVE=(HMM_DESCRIPTIVE[@] "$(_build_entry)")
+}

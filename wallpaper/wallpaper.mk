ef48e92c635e15f42f43a83e04bdb168072e62b6
Andreas Blaesius <skate4life@gmx.de>
2015-08-06 19:20:01 +0200

    vendor: add more Wallpaper by Cristian Giordano
    Change-Id: Ic9971a1e44e056279fd41111b0a35244c3b47d84

---
+++ wallpaper/wallpaper.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/SLIM05.png:system/media/wallpapers/SLIM05.png \
+    $(LOCAL_PATH)/slim-dark-fadingfire.jpg:system/media/wallpapers/slim-dark-fadingfire.jpg \
+    $(LOCAL_PATH)/slim-dark-fadingpurple.jpg:system/media/wallpapers/slim-dark-fadingpurple.jpg \
+    $(LOCAL_PATH)/slim-dark-greenyellow.jpg:system/media/wallpapers/slim-dark-greenyellow.jpg \
+    $(LOCAL_PATH)/slim-dark-totaldark.jpg:system/media/wallpapers/slim-dark-totaldark.jpg \
+    $(LOCAL_PATH)/slim-light-blue.jpg:system/media/wallpapers/slim-light-blue.jpg \
+    $(LOCAL_PATH)/slim-light-brightyellow.jpg:system/media/wallpapers/slim-light-brightyellow.jpg \
+    $(LOCAL_PATH)/slim-light-darkblue.jpg:system/media/wallpapers/slim-light-darkblue.jpg \
+    $(LOCAL_PATH)/slim-light-green.jpg:system/media/wallpapers/slim-light-green.jpg \
+    $(LOCAL_PATH)/slim-light-red.jpg:system/media/wallpapers/slim-light-red.jpg \
+    $(LOCAL_PATH)/slim-light-standard.jpg:system/media/wallpapers/slim-light-standard.jpg \
+    $(LOCAL_PATH)/slim-light-yellow.jpg:system/media/wallpapers/slim-light-yellow.jpg \
+    $(LOCAL_PATH)/slim-material01.jpg:system/media/wallpapers/slim-material01.jpg \
+    $(LOCAL_PATH)/slim-wire-blue.jpg:system/media/wallpapers/slim-wire-blue.jpg \
+    $(LOCAL_PATH)/slim-wire-grey.jpg:system/media/wallpapers/slim-wire-grey.jpg \
+    $(LOCAL_PATH)/slim-wire-purple.jpg:system/media/wallpapers/slim-wire-purple.jpg

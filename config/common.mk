302cfbfb0b56c0ae2263901d78cbd358cec2cb7d
herriojr <jherriott@cyngn.com>
2015-10-01 11:20:26 -0700

    Add libcore.net.MimeUtils Overrides
    This will allow us to add supported mimetypes in libcore.net.MimeUtils
    which in turn will allow more recognized mimetypes for anything that
    uses android.webkit.MimeTypeMap.
    
    Change-Id: I97febe17cff1d196f479b557fef3bca1b11bdd74
    Issue-Id: CYNGNOS-1176

--- config/common.mk
+++ config/common.mk
@@ -41,6 +41,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
 
+# Copy over added mimetype supported in libcore.net.MimeUtils
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml

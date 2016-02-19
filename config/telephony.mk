75d609f9c147a69715eec1c79b29a110f8c7b21d
razorloves <razorloves@gmail.com>
2016-02-18 23:47:47 -0600

    Revert "cm: add selective based spn (2/2)"
    Appears to be unused in CM13.
    
    This reverts commit 2cfe6b260bf743cbf3091095807b373037581181.
    
    Change-Id: Ia9beee776fd86f516ed9834f92530355a189f326
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- config/telephony.mk
+++ config/telephony.mk
@@ -6,10 +6,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# Selective SPN list for operator number who has the problem.
-PRODUCT_COPY_FILES += \
-    vendor/slim/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
-
 # Telephony packages
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver \

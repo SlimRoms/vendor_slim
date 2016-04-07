10471d5273393256bff21c4ad570490c3e1f515d
Josue Rivera <prbassplayer@gmail.com>
2016-04-07 05:30:36 +0200

    Slim: Cleanup tablet make files
      * Remove "common_full_tablet_wifionly.mk" as said devices should just call common.mk
      * Remove "common_full_tablet_lte" as said devices should call on "common_full_phone.mk
      * Remove "common_full_hybrid_wifionly.mk" deprecated
    
    Change-Id: I7ee4d67878c3c20c789c3a4f45aaefe1b455a9d7
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- config/common_full_tablet_lte.mk
+++
@@ -1,3 +0,0 @@
-# Inherit common stuff
-$(call inherit-product, vendor/slim/config/common.mk)
-$(call inherit-product, vendor/slim/config/common_apn.mk)
--- config/common_full_tablet_wifionly.mk
+++
@@ -1,2 +0,0 @@
-# Inherit common stuff
-$(call inherit-product, vendor/slim/config/common.mk)

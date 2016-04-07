06c056ecab39c3214419c3e3fc2a12add868c7f7
Josue Rivera <prbassplayer@gmail.com>
2016-04-07 05:30:36 +0200

    Slim: Cleanup tablet make files
      * Remove "common_full_tablet_wifionly.mk" as said devices should just call common.mk
      * Remove "common_full_tablet_lte" as said devices should call on "common_full_phone.mk
      * Remove "common_full_hybrid_wifionly.mk" deprecated
    
    Change-Id: I7ee4d67878c3c20c789c3a4f45aaefe1b455a9d7
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- config/common_full_hybrid_wifionly.mk
+++
@@ -1,8 +0,0 @@
-# Inherit common stuff
-$(call inherit-product, vendor/slim/config/common.mk)
-
-PRODUCT_PACKAGE_OVERLAYS += vendor/slim/overlay/tablet
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf

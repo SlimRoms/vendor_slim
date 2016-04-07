8f9a3c2442d7d5c1c52c79496f3600cc3aa06a1f
Josue Rivera <prbassplayer@gmail.com>
2016-04-07 04:47:36 +0200

    Slim: Cleanup and organize telephony make files
    Change-Id: I290d9d9774a4b4cde57f316c32c317bdace2c82a
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,7 +1,17 @@
 # Inherit common stuff
 $(call inherit-product, vendor/slim/config/common.mk)
-$(call inherit-product, vendor/slim/config/common_apn.mk)
 
-# SIM Toolkit
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# Telephony packages
 PRODUCT_PACKAGES += \
+    CellBroadcastReceiver \
+    messaging \
     Stk
+

fcd28acb96e806de69a2ef8d89d1f8fbb2a2ea62
Griffin Millender <griffinn.millender@gmail.com>
2016-08-18 16:43:58 -0500

    Add gmail and hangouts overlay and tweak makefiles
    Change-Id: Ibf115aa2eb54767e3967f6db176c3a99065fa00a

--- overlays/overlays.mk
+++ overlays/overlays.mk
@@ -11,15 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+PRODUCT_PACKAGES += \
+    GooglePackageInstallerOverlay \
+    GmailOverlay
 
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := GooglePackageInstallerOverlay
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_IGNORE_SUBDIR := true
-LOCAL_MODULE_PATH := $(TARGET_OUT)/slim/overlay
-include $(BUILD_PACKAGE)

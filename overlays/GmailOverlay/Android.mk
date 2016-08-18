fb15308c3dea6cf32fd5d9152844c709c8fb018f
Griffin Millender <griffinn.millender@gmail.com>
2016-08-18 16:43:58 -0500

    Add gmail overlay
    Change-Id: Ibf115aa2eb54767e3967f6db176c3a99065fa00a
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

---
+++ overlays/GmailOverlay/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := GmailOverlay
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_IGNORE_SUBDIR := true
+LOCAL_MODULE_PATH := $(TARGET_OUT)/slim/overlay
+include $(BUILD_PACKAGE)

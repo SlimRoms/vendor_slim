1b55068b328bff16941d4d09cd696b263355c74b
Daniel Koman <dankoman30@gmail.com>
2016-09-13 20:45:26 +0200

    add slack overlay to fix notification text color in dark mode
    Change-Id: I40b1f1660d4cc4cb491e3658e469aa1a30ec6b68

--- overlays/SlackOverlay/Android.mk
+++ overlays/SlackOverlay/Android.mk
@@ -12,9 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 
-PRODUCT_PACKAGES += \
-    GooglePackageInstallerOverlay \
-    GmailOverlay \
-    HangoutsOverlay \
-    SlackOverlay
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := SlackOverlay
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_IGNORE_SUBDIR := true
+LOCAL_MODULE_PATH := $(TARGET_OUT)/slim/overlay
+include $(BUILD_PACKAGE)

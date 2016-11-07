bffccf7f7632fe44c7f6fc4b54b90561416fadfa
Nicholas Flintham <nick@flinny.org>
2016-11-07 15:12:01 +0000

    Update backuptool for 7.1
    Change-Id: Iad7d314d4a76165b0eaa291b5e9cc8314231db0f

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -5,7 +5,7 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V=Slim-7.0
+export V=Slim-7.1
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp

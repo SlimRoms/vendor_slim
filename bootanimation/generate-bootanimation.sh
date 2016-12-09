bc8b1fa9cd3b62ae6b0cc0471bea26b709115a38
Nicholas Flintham <nick@flinny.org>
2016-12-09 21:36:13 +0000

    Merge in bootanimation generation improvements from CM
    Change-Id: I913e63cc0c27bc72270b0660bb902dc383ec2fd8

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 
-CWD=`pwd`
-WIDTH=$1
-HEIGHT=$(echo "$WIDTH/1.6" | bc)
-RWIDTH=$WIDTH
-RHEIGHT=$HEIGHT
-HALF_RES=$2
-if [ "$HALF_RES" = "true" ]; then
-    WIDTH=`expr $WIDTH / 2`
-    HEIGHT=`expr $HEIGHT / 2`
-fi
+WIDTH="$1"
+HEIGHT="$2"
+HALF_RES="$3"
+OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
 
-if [ -f "/usr/bin/convert" ]; then
-if [ -f "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" ]; then
-    echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
+if [ "$HEIGHT" -lt "$WIDTH" ]; then
+    SIZE="$HEIGHT"
 else
-RESOLUTION=""$WIDTH"x"$HEIGHT""
-
-mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part{0..4}
-tar xvfp "$PWD/vendor/slim/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/\$TAR_FILENAME\""
-# create desc.txt
-echo "$RWIDTH" "$RHEIGHT" 60 > "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
-cat "$PWD/vendor/slim/bootanimation/desc.txt" >> "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
-
-# create bootanimation.zip
-cd "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation"
-
-if [ ! -d "$ANDROID_PRODUCT_OUT/system/media" ]; then
-mkdir -p "$ANDROID_PRODUCT_OUT/system/media"
+    SIZE="$WIDTH"
 fi
 
-zip -r0 "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" .
-echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
+if [ "$HALF_RES" = "true" ]; then
+    IMAGESIZE=$(expr $SIZE / 2)
+else
+    IMAGESIZE="$SIZE"
+fi
 
-fi
-fi
+RESOLUTION=""$IMAGESIZE"x"$IMAGESIZE""
+
+for part_cnt in 0 1 2 3 4
+do
+    mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$part_cnt
+done
+tar xfp "vendor/slim/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"jpg:$OUT/bootanimation/\$TAR_FILENAME\""
+
+# Create desc.txt
+echo "$SIZE" "$SIZE" 30 > "$OUT/bootanimation/desc.txt"
+cat "vendor/slim/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
+
+# Create bootanimation.zip
+cd "$OUT/bootanimation"
+
+zip -qr0 "$OUT/bootanimation.zip" .
+

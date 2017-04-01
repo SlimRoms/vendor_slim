ac7000d7bba2715d74ad043b217003b7a30bc53a
Josue Rivera <prbassplayer@gmail.com>
2017-04-01 17:31:01 +0200

    slim: Split common_phone.mk into itself and telephony.mk
      * Allows for calling slave device slim.mk without calling common.mk twice
    
    Change-Id: I6aa70317d2d0f58bb820fad86cbc1f6302772169
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,15 +1,5 @@
 # Inherit common stuff
 $(call inherit-product, vendor/slim/config/common.mk)
 
-# World APN list
-PRODUCT_COPY_FILES += \
-    vendor/slim/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
-
-# Selective SPN list for operator number who has the problem.
-PRODUCT_COPY_FILES += \
-    vendor/slim/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
-
-# Telephony packages
-PRODUCT_PACKAGES += \
-    CellBroadcastReceiver \
-    Stk
+# Inherit telephony stuff
+$(call inherit-product, vendor/slim/config/telephony.mk)
---
+++ config/telephony.mk
@@ -0,0 +1,12 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    CellBroadcastReceiver \
+    Stk

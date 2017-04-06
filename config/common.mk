1543cae11e17ef0a15b7cbab9fef0b1053517e19
Josue Rivera <prbassplayer@gmail.com>
2017-04-06 22:04:55 +0200

    Revert "Revert "Slim: Build ThemeManager and init dir""
    This reverts commit ec9497d3a977d90961ce88ae4e7ca603376940a8.
    
    Revert "Revert "Slim: Add ThemeManager and OmsBackend sepolicy""
    
    This reverts commit 177c7a00d82b5d1d6aabfc57afc19fccda8e6f0c.
    
    Revert "Revert "Allow thememanager_app media_session_service to find""
    
    This reverts commit 2d0cbfac8d09ebca747d0b928c234ebb1bb46096.
    
    Change-Id: Ie8dc649262bff8bb56ea7a3c0b0e79b136f5b4fa

--- config/common.mk
+++ config/common.mk
@@ -89,6 +89,11 @@ PRODUCT_PACKAGES += \
     LatinIME \
     BluetoothExt
 
+# Theme Packages
+PRODUCT_PACKAGES += \
+    ThemeManager \
+    OmsBackend
+
 #    SlimFileManager removed until updated
 
 ifneq ($(DISABLE_SLIM_FRAMEWORK), true)

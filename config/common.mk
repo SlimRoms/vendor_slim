a5c06d293a0629d694a7c0bd81932894d685f90d
Adrian DC <radian.dc@gmail.com>
2017-07-24 16:41:53 +0200

    slim : remove old cm custom charger remnants
    Change-Id: Iafcfb683005f3b42fb5cb5f58a1603340a47ac78

--- config/common.mk
+++ config/common.mk
@@ -105,15 +105,6 @@ PRODUCT_PACKAGES += \
     fsck.ntfs \
     mount.ntfs
 
-# Custom off-mode charger
-ifneq ($(WITH_CM_CHARGER),false)
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    cm_charger_res_images \
-    font_log.png \
-    libhealthd.cm
-endif
-
 # exFAT tools
 PRODUCT_PACKAGES += \
     fsck.exfat \

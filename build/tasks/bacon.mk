71acc3aee31f038223d43f59d6207f194973fbe9
Nicholas Flintham <nick@flinny.org>
2017-08-25 14:09:28 +0100

    slim: bacon add slim ASCII art
    Change-Id: Ie973dfa0db0891f81f4b7ba66731284750929d6c

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -16,10 +16,31 @@
 # -----------------------------------------------------------------
 # Slim OTA update package
 
+# Build colors
+
+ifneq ($(BUILD_WITH_COLORS),0)
+  CL_SLIM="\033[38;5;24m"
+  CL_SGRY="\033[38;5;239m"
+  CL_RST="\033[0m"
+endif
+
 SLIM_TARGET_PACKAGE := $(PRODUCT_OUT)/Slim-$(SLIM_VERSION).zip
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(SLIM_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(SLIM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(SLIM_TARGET_PACKAGE).md5sum
-	@echo "Package Complete: $(SLIM_TARGET_PACKAGE)" >&2
+	@echo -e " "
+	@echo -e ${CL_SLIM}" "${CL_SLIM}
+	@echo -e ${CL_SLIM}"                              _______________________________________________  "${CL_SLIM}
+	@echo -e ${CL_SLIM}"                             /                                  slimroms.org | "${CL_SLIM}
+	@echo -e ${CL_SLIM}"                            /   _____________________________________________| "${CL_SLIM}
+	@echo -e ${CL_SLIM}"                           /   /                                               "${CL_SLIM}
+	@echo -e ${CL_SLIM}"                          /   /  _  _      "${CL_SGRY}" ______                             "${CL_SLIM}
+	@echo -e ${CL_SLIM}"                         /   /  | |(_)     "${CL_SGRY}"(_____ \                            "${CL_SLIM}
+	@echo -e ${CL_SLIM}"                        /   /   | | _ _____ "${CL_SGRY}"_____) )___  _____  ___            "${CL_SLIM}
+	@echo -e ${CL_SLIM}"  _____________________/   /    | || |     |"${CL_SGRY}"  __  // _ \|     |/___)           "${CL_SLIM}
+	@echo -e ${CL_SLIM}" |                        /     | || | | | |"${CL_SGRY}" |  \ \ |_| | | | |___ |           "${CL_SLIM}
+	@echo -e ${CL_SLIM}" |_______________________/       \_)_|_|_|_|"${CL_SGRY}"_|   |_\___/|_|_|_(___/            "${CL_SLIM}
+	@echo -e ${CL_SLIM}" "${CL_SLIM}
+	@echo -e ${CL_CYN}"Package Complete: $(SLIM_TARGET_PACKAGE)"${CL_RST}

37d6034abafc31077e7d61a7080fd8c01ec60111
Nicholas Flintham <nick@flinny.org>
2017-12-06 20:39:20 +0000

    Revert "soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable"
    This reverts commit 6fe64de9be2f10fe9b5fda20460429b2fc96510f.
    
    Change-Id: I77227c1129c09af95437e1d7cd14a2985e6d094a

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,15 +1,11 @@
 package android
 type Product_variables struct {
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
-
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 }
+
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,7 +4,6 @@ slim_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Slim": {'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)

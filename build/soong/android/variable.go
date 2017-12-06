2d8a733a1633157afde89f79df0047b75c97a809
Nicholas Flintham <nick@flinny.org>
2017-12-06 20:38:23 +0000

    Revert "soong_config: Add TARGET_USES_MEDIA_EXTENSIONS variable"
    This reverts commit 5bb7c0d6e03290b54526bb6ea65ba1b8fe31b4cc.
    
    Change-Id: Ifdbeee395128241323fc34290960c007fcf4c9bc

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -4,10 +4,6 @@ type Product_variables struct {
 		Cflags []string
 	}
 
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
@@ -15,6 +11,5 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 }

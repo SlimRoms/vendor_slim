4da4a76d7fda4737c945c19a2df4231e31eaa393
Arne Coucheron <arco68@gmail.com>
2017-12-27 11:36:09 +0100

    soong_config: Add TARGET_USES_QCOM_BSP_LEGACY
     * gzosp: partial, for some reason we had the rest
    
    Change-Id: Ia5ddfa263df71388b19c77d2509c29791177208d

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -48,6 +48,9 @@ type Product_variables struct {
 	Uses_metadata_as_fde_key struct {
 		Cflags []string
 	}
+	Uses_qcom_bsp_legacy struct {
+		Cppflags []string
+	}
 	Uses_qti_camera_device struct {
 		Cppflags []string
 		Shared_libs []string
@@ -91,6 +94,7 @@ type ProductVariables struct {
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_metadata_as_fde_key  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
+	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qcom_um_family  *bool `json:",omitempty"`
 	Uses_qcom_um_3_18_family  *bool `json:",omitempty"`
 	Uses_qcom_um_4_4_family  *bool `json:",omitempty"`

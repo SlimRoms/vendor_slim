fb4a3621be32b86b6c4e11062ec113b9faadb76f
Josue Rivera <prbassplayer@gmail.com>
2018-01-17 11:17:38 -0400

    extract_utils: Fix up to work on Slim sources
    Change-Id: Icb2a7cc689cdc64cda8e835980c31b368532fd1c
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -847,7 +847,7 @@ function extract() {
             # If OTA is block based, extract it.
             elif [ -a "$DUMPDIR"/system.new.dat ]; then
                 echo "Converting system.new.dat to system.img"
-                python "$CM_ROOT"/vendor/cm/build/tools/sdat2img.py "$DUMPDIR"/system.transfer.list "$DUMPDIR"/system.new.dat "$DUMPDIR"/system.img 2>&1
+                python "$SLIM_ROOT"/vendor/slim/build/tools/sdat2img.py "$DUMPDIR"/system.transfer.list "$DUMPDIR"/system.new.dat "$DUMPDIR"/system.img 2>&1
                 rm -rf "$DUMPDIR"/system.new.dat "$DUMPDIR"/system
                 mkdir "$DUMPDIR"/system "$DUMPDIR"/tmp
                 echo "Requesting sudo access to mount the system.img"

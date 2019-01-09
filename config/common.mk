33997c912c51033e8333185ade54785390f92100
dianlujitao <dianlujitao@lineageos.org>
2019-01-09 19:39:01 +0800

    Remove deprecated ro.device.cache_dir
    Change-Id: Ib8253a7b15b09283130d1f9d6ea649bd9e70aaf7

--- config/common.mk
+++ config/common.mk
@@ -157,12 +157,4 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 -include vendor/slim-priv/keys/keys.mk
 
-ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),)
-  PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.device.cache_dir=/data/cache
-else
-  PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.device.cache_dir=/cache
-endif
-
 $(call inherit-product-if-exists, vendor/extra/product.mk)

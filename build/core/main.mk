b4799d3ae3434208ad95bae0450b2e967d6845b9
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-03 12:25:55 +0100

    Never allow neverallows
    Change-Id: Ibf07e9e319d676666e3e7389720c24917e38a9ac

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,8 +1,3 @@
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Rules for MTK targets
 include $(TOPDIR)vendor/slim/build/core/mtk_target.mk
 

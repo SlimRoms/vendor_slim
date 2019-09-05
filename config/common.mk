dceaa3f9af8950ce12ff4a5ef2b2fd103b5542ba
Prema Chand Alugu <premaca@gmail.com>
2019-09-06 00:50:18 +0530

    vendor: Remove unused script/confs

--- config/common.mk
+++ config/common.mk
@@ -56,11 +56,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Don't export PS1 in /system/etc/mkshrc.
-PRODUCT_COPY_FILES += \
-    vendor/gzosp/prebuilt/common/etc/mkshrc:system/etc/mkshrc \
-    vendor/gzosp/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf
-
 # Fix Dialer
 #PRODUCT_COPY_FILES +=  \
 #    vendor/gzosp/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml

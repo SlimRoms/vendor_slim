cefdd5ce8625956b6fde6f3769942d0dee6b168b
Nicholas Flintham <nick@flinny.org>
2019-11-10 12:11:37 +0000

    slim: fix roomservice default_manifest path
    Change-Id: I84bc294c87dd1b22af14021f51ad6140be1dd6c7

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -39,11 +39,11 @@ except ImportError:
     urllib.parse = urlparse
     urllib.request = urllib2
 
-DEBUG = False
-default_manifest = ".repo/manifest.xml"
+DEBUG = True
+default_manifest = ".repo/manifests/slim_default.xml"
 
 custom_local_manifest = ".repo/local_manifests/slim_manifest.xml"
-custom_default_revision = "ng7.0"
+custom_default_revision = "10.0"
 custom_dependencies = "slim.dependencies"
 org_manifest = "SlimRoms"  # leave empty if org is provided in manifest
 org_display = "SlimRoms"  # needed for displaying

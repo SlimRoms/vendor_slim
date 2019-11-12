fa217395df6dec364d9cb79327bc8450e9874dcb
Nicholas Flintham <nick@flinny.org>
2019-11-12 22:36:27 +0000

    slim : temp dirty hack to fix wallpaper generation
    Change-Id: I94815c1d6108bf261fa50b9964b65fb9ca37c1ff

--- wallpaper/generate-wallpaper.sh
+++ wallpaper/generate-wallpaper.sh
@@ -7,6 +7,12 @@
 
 CWD=`pwd`
 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        OS=linux
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        OS=darwin
+fi
+
 #SOURCE IMAGE NAME
 BASE_NAME=default_wallpaper.png
 
@@ -39,21 +45,20 @@ XXXHDPI_FOLDER="$OUTPUT_PACKAGE"drawable-xxxhdpi/
 
 #CONVERT IMAGES
 mkdir -p "$HDPI_FOLDER" "$MDPI_FOLDER" "$NODPI_FOLDER" "$SW600DP_FOLDER" "$SW720DP_FOLDER" "$XHDPI_FOLDER" "$XXHDPI_FOLDER" "$XXXHDPI_FOLDER"
-convert $BASE_WALLPAPER -resize $HDPI_RES^ -gravity center -extent $HDPI_RES $HDPI_FOLDER/$BASE_NAME
+prebuilts/tools-slim/${OS}-x86/bin/mogrify -resize $MDPI_RES^ -gravity center -extent $HDPI_RES -path $HDPI_FOLDER/ $BASE_WALLPAPER
 echo "$HDPI_FOLDER$BASE_NAME created"
-convert $BASE_WALLPAPER -resize $MDPI_RES^ -gravity center -extent $MDPI_RES $MDPI_FOLDER/$BASE_NAME
+prebuilts/tools-slim/${OS}-x86/bin/mogrify -resize $MDPI_RES^ -gravity center -extent $MDPI_RES -path $MDPI_FOLDER/ $BASE_WALLPAPER
 echo "$MDPI_FOLDER$BASE_NAME created"
-convert $BASE_WALLPAPER -resize $NODPI_RES^ -gravity center -extent $NODPI_RES $NODPI_FOLDER/$BASE_NAME
+prebuilts/tools-slim/${OS}-x86/bin/mogrify -resize $NODPI_RES^ -gravity center -extent $NODPI_RES -path $NODPI_FOLDER/ $BASE_WALLPAPER
 echo "$NODPI_FOLDER$BASE_NAME created"
-convert $BASE_WALLPAPER -resize $SW600DP_RES^ -gravity center -extent $SW600DP_RES $SW600DP_FOLDER/$BASE_NAME
+prebuilts/tools-slim/${OS}-x86/bin/mogrify -resize $SW600DP_RES^ -gravity center -extent $SW600DP_RES -path $SW600DP_FOLDER/ $BASE_WALLPAPER
 echo "$SW600DP_FOLDER$BASE_NAME created"
-convert $BASE_WALLPAPER -resize $SW720DP_RES^ -gravity center -extent $SW720DP_RES $SW720DP_FOLDER/$BASE_NAME
+prebuilts/tools-slim/${OS}-x86/bin/mogrify -resize $SW720DP_RES^ -gravity center -extent $SW720DP_RES -path $SW720DP_FOLDER/ $BASE_WALLPAPER
 echo "$SW720DP_FOLDER$BASE_NAME created"
-convert $BASE_WALLPAPER -resize $XHDPI_RES^ -gravity center -extent $XHDPI_RES $XHDPI_FOLDER/$BASE_NAME
+prebuilts/tools-slim/${OS}-x86/bin/mogrify -resize $XHDPI_RES^ -gravity center -extent $XHDPI_RES -path $XHDPI_FOLDER/ $BASE_WALLPAPER
 echo "$XHDPI_FOLDER$BASE_NAME created"
-convert $BASE_WALLPAPER -resize $XXHDPI_RES^ -gravity center -extent $XXHDPI_RES $XXHDPI_FOLDER/$BASE_NAME
+prebuilts/tools-slim/${OS}-x86/bin/mogrify -resize $XXHDPI_RES^ -gravity center -extent $XXHDPI_RES -path $XXHDPI_FOLDER/ $BASE_WALLPAPER
 echo "$XXHDPI_FOLDER$BASE_NAME created"
-convert $BASE_WALLPAPER -resize $XXXHDPI_RES^ -gravity center -extent $XXXHDPI_RES $XXXHDPI_FOLDER/$BASE_NAME
+prebuilts/tools-slim/${OS}-x86/bin/mogrify -resize $XXXHDPI_RES^ -gravity center -extent $XXXHDPI_RES -path $XXXHDPI_FOLDER/ $BASE_WALLPAPER
 echo "$XXXHDPI_FOLDER$BASE_NAME created"
 echo "ALL DONE!"
-
